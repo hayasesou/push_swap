87a9ecf9d4c3e6d20e9810114aff21a1da8dc2cf
Hayase Fukushi <hfukushi@c1r24s4.42tokyo.jp>
2023-10-01 13:04:04 +0900

    hello push_swap

--- srcs/quick_sort.c
+++ srcs/quick_sort.c
@@ -6,7 +6,7 @@
 /*   By: hfukushi <hfukushi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 15:45:17 by hfukushi          #+#    #+#             */
-/*   Updated: 2023/10/01 12:22:31 by hfukushi         ###   ########.fr       */
+/*   Updated: 2023/10/01 12:59:06 by hfukushi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,51 +24,44 @@ void print_stack(t_cd_list *stack)
 	dprintf(STDERR_FILENO, "]\n");
 }
 
-void	stack_b_quick_sort(int av_num, t_cd_list **stack_b, t_cd_list **stack_a, int *group_id_max)
+static void handle_stack_under13(int av_num, t_cd_list **stack_a, t_cd_list **stack_b)
 {
-	int	pivot;
-	int	i;
-	int count_stack_b_node;
-	int count_ra;
-	int group_id;
+	stack_b_insertion_sort(av_num, stack_b, stack_a);
+	if (av_num >3)
+	{
+		while ((*stack_a)->group_id == (*stack_a)->next->group_id)
+			ft_ra(stack_a);
+			ft_ra(stack_a);
+	}
+	while ((*stack_b) != NULL)
+	{
+		push_x2y(stack_b, stack_a, A);
+		ft_ra(stack_a);
+	}
+	return ;
+}
 
-	if (av_num < 12)
-	{
-		stack_b_insertion_sort(av_num, stack_b, stack_a);
-		if (av_num >3)
-		{
-			while ((*stack_a)->group_id == (*stack_a)->next->group_id)
-			{
-				ft_ra(stack_a);
-			}
-			ft_ra(stack_a);
-		}
-		while ((*stack_b) != NULL)
-		{
-			push_x2y(stack_b, stack_a, A);
-			ft_ra(stack_a);
-		}
-		return ;
-	}
-	pivot = get_pivot(av_num, stack_b);
-	i = -1;
+static int separate_stack_b(int pivot, t_cd_list **stack_a, t_cd_list **stack_b, int *group_id_max)
+{
+	int count_stack_b_node;
+
 	count_stack_b_node = 0;
-	while (++i < av_num)
+	if ((*stack_b)->content >= pivot)
 	{
-		if ((*stack_b)->content >= pivot)
-		{
-			(*stack_b)->group_id = *group_id_max + 1;
-			push_x2y(stack_b, stack_a, A);
-		}
-		else
-		{
-			ft_rb(stack_b);
-			count_stack_b_node++;
-		}
+		(*stack_b)->group_id = *group_id_max + 1;
+		push_x2y(stack_b, stack_a, A);
 	}
-	group_id = *group_id_max;
-	*group_id_max += 1;
-	stack_b_quick_sort(count_stack_b_node, stack_b, stack_a, group_id_max);
+	else
+	{
+		ft_rb(stack_b);
+		count_stack_b_node++;
+	}
+	return (count_stack_b_node);
+}
+
+static void set_next_block2stack_b(t_cd_list **stack_a, t_cd_list **stack_b, int group_id, int *group_id_max)
+{
+	int count_ra;
 
 	count_ra = 0;
 	while ((*stack_a)->group_id == group_id + 1)
@@ -78,6 +71,29 @@ void	stack_b_quick_sort(int av_num, t_cd_list **stack_b, t_cd_list **stack_a, in
 	}
 	if ((*stack_b) != NULL)
 		stack_b_quick_sort(count_ra, stack_b, stack_a, group_id_max);
+}
+
+void	stack_b_quick_sort(int av_num, t_cd_list **stack_b, t_cd_list **stack_a, int *group_id_max)
+{
+	int	pivot;
+	int	i;
+	int count_stack_b_node;
+	int group_id;
+
+	if (av_num < 12)
+	{
+		handle_stack_under13(av_num, stack_a, stack_b);
+		return ;
+	}
+	pivot = get_pivot(av_num, stack_b);
+	i = -1;
+	count_stack_b_node = 0;
+	while (++i < av_num)
+		count_stack_b_node += separate_stack_b(pivot, stack_a, stack_b, group_id_max);
+	group_id = *group_id_max;
+	*group_id_max += 1;
+	stack_b_quick_sort(count_stack_b_node, stack_b, stack_a, group_id_max);
+	set_next_block2stack_b(stack_a, stack_b, group_id, group_id_max);
 }
 
 
